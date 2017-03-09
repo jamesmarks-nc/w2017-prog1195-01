2fc1caebccae06bff307de98c11d84ec1c577018
jamesmarks <jamesgmarks@gmail.com>
2017-03-08 19:53:51 -0500

    End of class files

--- week-9/example-calc/calc.js
+++ week-9/example-calc/calc.js
@@ -5,30 +5,44 @@ var calcState = {
 
 var txtNumber = document.getElementById("currentNumber");
 
-function handleButtonClick(label) {
-  if(!isNaN(parseInt(label))) {
+function handleDigitClick(digit) {
+    // but if my number on the screen is currently 0 erase it first.
     if(txtNumber.value === '0') txtNumber.value = '';
-    txtNumber.value += label;
-  } else if(label === '.') {
-    if(txtNumber.value.indexOf('.') === -1) {
-      txtNumber.value += '.';
-    }
-  } else {
-    var number = parseFloat(txtNumber.value);
-    if(calcState.previousValue !== null && calcState.activeOperation !== null) {
-      var operation = calcState.previousValue + calcState.activeOperation + number
-      calcState.previousValue = eval(operation);
-      document.getElementById('peek').innerText = operation + "=" + calcState.previousValue;
-    } else {
-      calcState.previousValue = number;
-    }
-    txtNumber.value = "0";
-    if(label === '=') {
-      txtNumber.value = calcState.previousValue;
-      calcState.previousValue = null;
-      calcState.activeOperation = null;
-    } else {
-      calcState.activeOperation = label;
-    }
+    txtNumber.value += digit;
+}
+
+function handleDotClick() {
+  if(txtNumber.value.indexOf('.') === -1) {
+    txtNumber.value += '.';
   }
+}
+
+function handlePrevious() {
+  if(calcState.previousValue !== null && calcState.activeOperation !== null) {
+    var operation = calcState.previousValue + calcState.activeOperation + number
+    calcState.previousValue = eval(operation);
+    document.getElementById('peek').innerText = operation + "=" + calcState.previousValue;
+  } else {
+    calcState.previousValue = number;
+  }
+  txtNumber.value = "0";
+}
+
+function handleEqualButton() {
+  if(operator === '=') {
+    txtNumber.value = calcState.previousValue;
+    calcState.previousValue = null;
+    calcState.activeOperation = null;
+  } else {
+    calcState.activeOperation = operator;
+  }
+}
+
+function handleOperatorClick(operator) {
+  // get the currently displayed number
+  var number = parseFloat(txtNumber.value);
+  // take care of any operations in the queue.
+  handlePrevious();
+  // if this is the equals operator -> display the result.
+  handleEqualButton(operator);
 }
--- week-9/example-calc/date.js
+++ week-9/example-calc/date.js
@@ -1,6 +1,34 @@
 var workingDate = new Date();
 
 var currentDateTextbox = document.getElementById('currentDate');
-currentDateTextbox.value = workingDate.toDateString();
 
-displayDate();
+function displayDate() {
+  currentDateTextbox.value = workingDate.toDateString() 
+    + ' ' + workingDate.getHours()
+    + ":" + workingDate.getMinutes();
+}
+
+function increaseYear() {
+  var year = workingDate.getFullYear();
+  year = year + 1;
+  workingDate.setFullYear(year);
+  displayDate();
+}
+
+function decreaseYear() {
+  var year = workingDate.getFullYear();
+  year = year - 1;
+  workingDate.setFullYear(year);
+  displayDate();
+}
+
+function increaseMonth() {
+  workingDate.setMonth(workingDate.getMonth() + 1);
+  displayDate();
+}
+
+function decreaseMonth() {
+  var month = workingDate.getMonth();
+  month = month - 1;
+  workingDate.setMonth(month);
+}
