aba6a04ea4f20a9f01e40cfdff533fe38ba2e26d
jamesmarks <jamesgmarks@gmail.com>
2017-04-12 13:48:39 -0400

    week 14, day 2, start

---
+++ week-14/working-with-data/script.js
@@ -0,0 +1,17 @@
+
+var locationButton = document.getElementById("locationButton");
+
+// Geolocation service
+// Info: http://ip-api.com/docs/api:json
+// API:  http://ip-api.com/json
+
+// Weather service
+// Info: https://openweathermap.org/api
+// API:  http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=c1f700662e14d6debd730ff65414acd9
+
+var exploreButton = document.getElementById("exploreButton");
+
+// Info: https://swapi.co/
+// API:  http://swapi.co/api/people/
+// API:  http://swapi.co/api/starships/
+// API:  http://swapi.co/api/planets/
